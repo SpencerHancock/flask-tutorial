2ce1c17f5c18cdb569eac4f654e9866ccc0e2d77
SpencerHancock <spencerjhancock@gmail.com>
2018-08-16 10:12:58 -0700

    third iteration

--- flaskr/db.py
+++ flaskr/db.py
@@ -8,28 +8,31 @@ from flask.cli import with_appcontext
 def get_db():
 	if "db" not in g:
 		g.db = sqlite3.connect(
-			current_app.congig["DATABASE"],
+			current_app.config["DATABASE"],
 			detect_types=sqlite3.PARSE_DECLTYPES)
-		g.db.row_factory = sqlite3.row_factory
+		g.db.row_factory = sqlite3.Row
 
 	return g.db
 
- def close_db(e=None):
+def close_db(e=None):
  	db = g.pop("db", None)
 
  	if db is not None:
  		db.close()
 
- def init_db():
+def init_db():
  	db = get_db()
 
  	with current_app.open_resource("schema.sql") as f:
  		db.executescript(f.read().decode("utf8"))
 
- @click.command("init_db")
+@click.command("init-db")
 
- @with_appcontext
- def init_db_command():
- 	init_db()
- 	click.echo("initialized the database.")
- 	
+@with_appcontext
+def init_db_command():
+	init_db()
+	click.echo("initialized the database.")
+
+def init_app(app):
+	app.teardown_appcontext(close_db)
+	app.cli.add_command(init_db_command)
